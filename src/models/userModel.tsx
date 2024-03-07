b303b7a89fc9e8b01be8ac150c195115f4ec42d4
Parag0712 <paragvadgama123@gmail.com>
2024-03-07 23:17:25 +0530

    user model created

---
+++ src/models/userModel.tsx
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:[true,"Username Already exists"],
+        required: [true, "Username is required"],
+    },
+    email:{
+        type:String,
+        required: [true, "Email is required"],
+        unique:[true,"Email Already exists"],
+    },
+    password:{
+        type:String,
+        required: [true, "Password is required"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+})
+
+const User = mongoose.models.User || mongoose.model("User",userSchema);
