a4471fe6484658a34ec25ba031ddeedfbbd87111
Parag0712 <paragvadgama123@gmail.com>
2024-03-07 22:05:56 +0530

    in header link added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
+"use client"
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 function Header() {
+    const router = useRouter();
     return (
         <div className="relative w-full bg-white">
             <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-3 sm:px-6 lg:px-8">
@@ -100,12 +103,14 @@ function Header() {
                 <div className="hidden space-x-2 lg:block">
                     <button
                         type="button"
+                        onClick={()=>router.push("/register")}
                         className="rounded-md bg-transparent px-3 py-2 text-sm font-semibold text-black hover:bg-black/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                     >
                         Sign In
                     </button>
                     <button
                         type="button"
+                        onClick={()=>router.push("/login")}
                         className="rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                     >
                         Log In
